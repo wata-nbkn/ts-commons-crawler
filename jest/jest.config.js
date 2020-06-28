e994bb3908627cede2a77f2c967d001eaf732d6c
NBKN <knt.watanab@gmail.com>
2020-06-28 17:10:01 +0900

    fix jest config

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   rootDir: '../',
   moduleFileExtensions: ['js', 'ts'],
-  moduleNameMapper: {
-    '^types$': '<rootDir>/src/types',
-    '^consts$': '<rootDir>/src/consts',
-    '^utils$': '<rootDir>/src/utils',
-    '^utils/(.+)': '<rootDir>/src/utils/$1',
-    '^fileUtils$': '<rootDir>/src/fileUtils',
-  },
   preset: 'ts-jest',
   testRegex: '/__tests__/.*\\.spec\\.ts$',
   setupFilesAfterEnv: ['<rootDir>/jest/jest.setup.js'],
